0392f02ab603ca2f991965a946e90de413a52f40
An Li <anli@Ans-Mac-mini.local>
2017-04-03 17:34:31 +0800

    devise views

--- db/migrate/20170403024341_add_user_name_to_user.rb
+++ db/migrate/20170403024341_add_user_name_to_user.rb
@@ -1,5 +1,5 @@
 class AddUserNameToUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :name, :string, default: "no name"
+    add_column :users, :name, :string, default: "Anonym"
   end
 end
