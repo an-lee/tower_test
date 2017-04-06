877d5e9eec284afbd14f9b55720483e988f69171
An Li <anli@Ans-Mac-mini.local>
2017-04-07 06:55:04 +0800

    events index show only wents from participated team and participated project

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,8 +3,12 @@ class EventsController < ApplicationController
 
   def index
     @team = current_user.participated_teams.find(params[:team_id])
-    @events = @team.events.includes(:project, :todo)
-    @events = @events.recent.paginate(:page => params[:page], :per_page => 50)
+    @projects = @team.projects
+    @projects_joined = current_user.participated_projects
+    @projects = @projects & @projects_joined
+    @events = @projects.collect{ |project| project.events }.flatten
+    @events = @events.sort_by(&:id).reverse
+    @events = @events.paginate(:page => params[:page], :per_page => 50)
   end
 
   private
